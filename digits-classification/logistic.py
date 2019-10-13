82c8140b78b6764eeed937c26d0f6674358917ed
Labib <labibc01@gmail.com>
2019-10-13 14:30:00 -0400

    added digits classification work; comparing knn and logistic regression model

---
+++ digits-classification/logistic.py
@@ -0,0 +1,113 @@
+""" Methods for doing logistic regression."""
+
+import numpy as np
+from utils import sigmoid, load_valid
+
+def logistic_predict(weights, data):
+    """
+    Compute the probabilities predicted by the logistic classifier.
+
+    Note: N is the number of examples and
+          M is the number of features per example.
+
+    Inputs:
+        weights:    (M+1) x 1 vector of weights, where the last element
+                    corresponds to the bias (intercepts).
+        data:       N x M data matrix where each row corresponds
+                    to one data point.
+    Outputs:
+        y:          :N x 1 vector of probabilities. This is the output of the classifier.
+    """
+
+    # TODO: Finish this function
+    # data = valid_inputs
+    # z = w^Tx +b
+    # y = sigmoid(z)
+
+    x = np.c_[data, np.ones(data.shape[0])]
+    z = np.dot(x, weights)
+    y = sigmoid(z)
+
+    return y
+
+def evaluate(targets, y):
+    """
+    Compute evaluation metrics.
+    Inputs:
+        targets : N x 1 vector of targets.
+        y       : N x 1 vector of probabilities.
+    Outputs:
+        ce           : (scalar) Cross entropy. CE(p, q) = E_p[-log q]. Here we want to compute CE(targets, y)
+        frac_correct : (scalar) Fraction of inputs classified correctly.
+    """
+    # TODO: Finish this function
+    ce = -1*np.sum(targets*np.log(y))
+    apply_thresh = (y > 0.5)
+    frac_correct = (targets == apply_thresh).mean()
+    return ce, frac_correct
+
+def logistic(weights, data, targets, hyperparameters):
+    """
+    Calculate negative log likelihood and its derivatives with respect to weights.
+    Also return the predictions.
+
+    Note: N is the number of examples and
+          M is the number of features per example.
+
+    Inputs:
+        weights:    (M+1) x 1 vector of weights, where the last element
+                    corresponds to bias (intercepts).
+        data:       N x M data matrix where each row corresponds
+                    to one data point.
+        targets:    N x 1 vector of targets class probabilities.
+        hyperparameters: The hyperparameters dictionary.
+
+    Outputs:
+        f:       The sum of the loss over all data points. This is the objective that we want to minimize.
+        df:      (M+1) x 1 vector of derivative of f w.r.t. weights.
+        y:       N x 1 vector of probabilities.
+    """
+
+    # TODO: Finish this function
+    y = logistic_predict(weights, data)
+    x = np.c_[data, np.ones(data.shape[0])]
+    f, frac_correct = evaluate(targets, y)
+    df = x.transpose().dot(y - targets)
+    return f, df, y
+
+
+def logistic_pen(weights, data, targets, hyperparameters):
+    """
+    Calculate negative log likelihood and its derivatives with respect to weights.
+    Also return the predictions.
+
+    Note: N is the number of examples and
+          M is the number of features per example.
+
+    Inputs:
+        weights:    (M+1) x 1 vector of weights, where the last element
+                    corresponds to bias (intercepts).
+        data:       N x M data matrix where each row corresponds
+                    to one data point.
+        targets:    N x 1 vector of targets class probabilities.
+        hyperparameters: The hyperparameters dictionary.
+
+    Outputs:
+        f:             The sum of the loss over all data points. This is the objective that we want to minimize.
+        df:            (M+1) x 1 vector of derivative of f w.r.t. weights.
+    """
+
+    # TODO: Finish this function
+    f, df, y = logistic(weights, data, targets, hyperparameters)
+    # TODO: exlcude last row of weights somehow
+    f = f + (hyperparameters['weight_regularization']/2)*np.sum(weights**2)
+    df = df + hyperparameters['weight_regularization']*weights
+    return f, df, y
+
+if __name__ == '__main__':
+    valid_inputs, valid_targets = load_valid()
+    weights = np.random.rand(valid_inputs.shape[1]+1, 1)
+    y = logistic_predict(weights, valid_inputs)
+    ce, frac_correct = evaluate(valid_targets, y)
+    f, df, y = logistic(weights, valid_inputs, valid_targets, None)
+
---
+++ digits-classification/run_knn.py
@@ -0,0 +1,68 @@
+import numpy as np
+from utils import *
+from sklearn.metrics import accuracy_score
+from l2_distance import l2_distance
+import matplotlib.pyplot as plt
+
+LEARNING_RATE = [1,3,5,7,9]
+def run_knn(k, train_data, train_labels, valid_data):
+    """Uses the supplied training inputs and labels to make
+    predictions for validation data using the K-nearest neighbours
+    algorithm.
+
+    Note: N_TRAIN is the number of training examples,
+          N_VALID is the number of validation examples, 
+          and M is the number of features per example.
+
+    Inputs:
+        k:            The number of neighbours to use for classification 
+                      of a validation example.
+        train_data:   The N_TRAIN x M array of training
+                      data.
+        train_labels: The N_TRAIN x 1 vector of training labels
+                      corresponding to the examples in train_data 
+                      (must be binary).
+        valid_data:   The N_VALID x M array of data to
+                      predict classes for.
+
+    Outputs:
+        valid_labels: The N_VALID x 1 vector of predicted labels 
+                      for the validation data.
+    """
+
+    dist = l2_distance(valid_data.T, train_data.T)
+    nearest = np.argsort(dist, axis=1)[:,:k]
+
+    train_labels = train_labels.reshape(-1)
+    valid_labels = train_labels[nearest]
+
+    # note this only works for binary labels
+    valid_labels = (np.mean(valid_labels, axis=1) >= 0.5).astype(np.int)
+    valid_labels = valid_labels.reshape(-1,1)
+
+    return valid_labels
+
+if __name__ == '__main__':
+    train_inputs, train_targets = load_train()
+    test_inputs, test_targets = load_test()
+    valid_inputs, valid_targets = load_valid()
+
+    val_acc = []
+    test_acc = []
+    for k in LEARNING_RATE:
+        v_prediction = run_knn(k, train_inputs, train_targets, valid_inputs)
+
+        t_prediction = run_knn(k, train_inputs, train_targets,test_inputs)
+        val_accuracy = accuracy_score(valid_targets, v_prediction)
+        test_accuracy = accuracy_score(test_targets, t_prediction)
+        val_acc.append(val_accuracy)
+        test_acc.append(test_accuracy)
+
+    fig = plt.figure()
+    plt.xlabel("Learning Rate")
+    plt.ylabel("Classification Rate")
+    plt.plot(LEARNING_RATE, val_acc, color='tab:red',label='Validation Accuracy')
+    plt.plot(LEARNING_RATE, test_acc, color='tab:blue', label='Test Accuracy')
+    plt.legend()
+    fig.savefig('classification_rates.png')
+
